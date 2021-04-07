0e7f1bd0fd4659ba77b9b1bc89f3ff8b212dd37b
Leonardo Omaña <leotangram@gmail.com>
2021-04-07 17:19:43 -0500

    Displaying Single Items, Routing and SEO

---
+++ pages/product/[id].js
@@ -0,0 +1,10 @@
+import SingleProduct from '../../components/SingleProduct'
+
+const SingleProductPage = ({ query }) => {
+
+  return (
+    <SingleProduct id={query.id} />
+  )
+}
+
+export default SingleProductPage
