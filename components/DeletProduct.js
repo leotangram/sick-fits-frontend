d6875144edd42027b4743d22f7609eafc9310d10
Leonardo Omaña <leotangram@gmail.com>
2021-04-09 10:26:07 -0500

    Evicting Items from the Apollo Cache

--- components/DeletProduct.js
+++ components/DeletProduct.js
@@ -10,11 +10,16 @@ const DELETE_PRODUCT_MUTATION = gql`
   }
 `
 
+const update = (cache, payload) => {
+  cache.evict(cache.identify(payload.data.deleteProduct))
+}
+
 const DeletProduct = ({ id, children }) => {
   const [deleteProduct, {error, loading}] = useMutation(DELETE_PRODUCT_MUTATION, {
     variables: {
-      id
-    }
+      id,
+    },
+    update
   })
 
   const handleDelete = () => {
